445006a7650f47106b3786187e60108b7fc6b6a1
iPMO <thomas.krueger@cms.hu-berlin.de>
2021-06-30 17:26:06 +0200

    project dataframe functionality added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,47 @@
 require 'daru'
 
-class Project 
-  @documents = nil 
-  @default_data_frame = nil 
-  @project_data_frame = nil
+class Project
+
+  attr_accessor :tuples, :multi_index, :vector_pre, :vector_init, :vector_sub1, :vector_sub2, :vector_final, :default_dataframe
 
   def initialize(project)
-    @documents = Hash.new
-    Document.where(project_name: project).each do |d| 
+   @default_dataframe = nil 
+   @project_dataframe = nil
+
+   # Variables for creating data frame structure
+   @tuples = [
+    [:corporate,0],
+    [:direct,1],
+    [:manage,2],
+    [:manage,3],
+    [:deliver,4]
+   ]
+
+   @multi_index = Daru::MultiIndex.from_tuples(@tuples)
+
+   @vector_pre = [0.0,0.1,0.2,0.3,0.4]
+   @vector_init =[1.0,1.1,1.2,1.3,1.4]
+   @vector_sub1 =[2.0,2.1,2.2,2.3,2.4]
+   @vector_sub2 =[3.0,3.1,3.2,3.3,3.4]
+   @vector_final=[4.0,4.1,4.2,4.3,4.4]
+
+   @order_mi = Daru::MultiIndex.from_tuples([
+    [:pre,0],
+    [:initiation,1],
+    [:subsequent,2],
+    [:subsequent,3],
+    [:final,4]
+    ])
+
+   create_default_dataframe
+
+    if project != nil 
+     @documents = Hash.new
+     Document.where(project_name: project).each do |d| 
      @documents.store(d.id,d.read_attribute(:process_step)) 
+     end
+    else
+      @document = nil
     end
   end
 
@@ -16,25 +49,35 @@ class Project
     @documents.each {|key, value| puts "key: #{key} => value: #{value}"} 
   end
 
-  def self.get_default_data_frame
-    @default_data_frame = Daru::DataFrame.new(
-      {
-        'Pre' => ['0.0.z','0.1.z','0.2.z','0.3.z','0.4.z'],
-        'Initiation' => ['1.0.z','1.1.z','1.2.z','1.3.z','1.4.z'],
-        'Subsequent1' => ['2.0.z','2.1.z','2.2.z','2.3.z','2.4.z'],
-        'Subsequent2' => ['3.0.z','3.1.z','3.2.z','3.3.z','3.4.z'],
-        'Final' => ['4.0.z','4.1.z','4.2.z','4.3.z','4.4.z']
-      },
-      index: ['Corporate', 'Direction', 'Management1','Management2', 'Delivery']
-    )
-   @default_data_frame 
+  # Thias method is replacing the default_dataframe values
+  # in format x.y.z with the link to an document
+  def get_project_dataframe
+    if @documents != nil
+     @project_dataframe = @default_dataframe.clone_structure
+     puts "processing #{@documents}"
+     @documents.each {|key, value|  
+       arr = value.split(/\./) 
+       puts "array = #{arr.to_s}"
+       @project_dataframe[arr[0].to_i][arr[1].to_i] = key 
+    }
+    else
+      @project_dataframe = @default_dataframe
+    end
+    @project_dataframe
   end
 
-  def get_documents_hash
-   @documents
+  def create_default_dataframe
+    puts "tuples are #{get_tuples}"
+    puts "vector_pre = #{get_vector_pre}"
+   @default_dataframe = Daru::DataFrame.new([@vector_pre, @vector_init, @vector_sub1, @vector_sub2, @vector_final], order: @order_mi, index: @multi_index)
   end
 
-  def get_project_data_frame
-
+  def get_tuples
+   @tuples
   end
+
+  def get_vector_pre
+   @vector_pre
+  end
+
 end
